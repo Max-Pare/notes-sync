f66ce5df5a3fe67a2718a4083549e71b19046932
Max-Pare <resilientcrab@proton.me>
2025-07-08 21:25:00 +0200

    did nothing

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ repo_dir = ''
 
 def main():
     print('\nInitializing ...\n'\
-    '**WARNING** webcam scan did not detect any trans or trans-validating flags in frame, '\
+    '**WARNING**: webcam scan did not detect any trans or trans-validating flags in frame, '\
         'program functionality will be limited until you confirm your ally status.')
     if FAKE_ARG:
         if len(sys.argv) > 1:
@@ -20,25 +20,26 @@ def main():
             sys.argv.append(FAKE_ARG)
     print(sys.argv[1]) #debug
     repo_dir = sys.argv[1]
-    r_validate_dir(repo_dir)
-    repo_path = repo_prompt_create()
+    while not validate_dir(repo_dir):
+        repo_dir = prompt_create_repo()
     print(repo_path)
 
-def r_validate_dir(_path): # Methods beginning with r_ raise an exception 
-    if not os.path.isdir(_path):
-        if os.path.isfile(_path):
-            raise FileNotFoundError('Not a directory.')
-        raise FileNotFoundError('Directory does not exist.')
-
-def repo_prompt_create() -> tuple[bool, pathlib.Path]:
-    data = input('Enter complete repo path (e.g.: /home/notes/dirt shopping list): ')
-    if len(data) > 1024: raise Exception('Path too long.') # this ain't C++ but whatever it's probably a good idea
+def prompt_create_repo() -> tuple[bool, pathlib.Path]:
+    data = input('Enter complete repo path (e.g.: /home/notes/dirt shopping list/): ')
+    if len(data) > 1024: raise Exception('Path too long.') # probably a good idea
     try:
         _p = pathlib.Path(data)
     except Exception as e:
         raise Exception(f'Invalid repo path ({str(e)})')
     return _p
     
+def validate_dir(_path):
+    if not _path: return False 
+    if not os.path.isdir(_path):
+        if os.path.isfile(_path):
+            raise FileNotFoundError('The path specified is a file, not a directory')
+        return False
+    return True
 
 if __name__ == '__main__':
     main()
