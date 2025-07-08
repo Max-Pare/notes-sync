38738859caea9bad37012e3a9a3c08e153214e4b
Max-Pare <resilientcrab@proton.ne>
2025-07-09 00:15:39 +0200

    made experience more validating

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import colorama
 from colorama import Fore, Back, Style
+import random
 
 colorama.init()
 VERSION = '0.0.1'
@@ -8,8 +9,9 @@ def main():
     cprint('#' * 28, Fore.YELLOW)
     cprint('   ' + 'notesync version '+ VERSION, Fore.YELLOW)
     cprint('#' * 28, Fore.YELLOW)
-    cprint('**WARNING** Webcam scan did not detect any trans or trans-validating flags in frame, '\
+    cprint('**WARNING** Webcam scan did not detect any trans or trans-adjacent pride flags in frame, '\
         'program functionality will be limited until your ally status is confirmed.', Fore.RED)
+    if random.random() <= .43: print(' ' * random.randint(8, 65) + 'ACK!')
 
 def cprint(string:str, col):
     print(col + string + Fore.WHITE)
