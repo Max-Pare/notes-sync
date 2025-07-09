1cff84608bb019bb1f9d9429c65b1e145cb1d2b1
Max-Pare <resilientcrab@proton.ne>
2025-07-09 21:48:18 +0200

    commit

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import colorama
 from colorama import Fore, Back, Style
 import random
+import pathlib 
+
 
 colorama.init()
 VERSION = '0.0.1'
+
+local_repo = ''
+
 
 def main():
     cprint('#' * 28, Fore.YELLOW)
@@ -15,6 +20,26 @@ def main():
 
 def cprint(string:str, col):
     print(col + string + Fore.WHITE)
+
+def prompt_create_repo() -> tuple[bool, pathlib.Path]:
+    data = input('Enter complete repo path (e.g.: /home/notes/dirt shopping list/): ')
+    if len(data) > 1024:
+        # probably a good idea
+        cprint('Path too long', Fore.RED)
+    try:
+        _p = pathlib.Path(data)
+    except Exception as e:
+        raise Exception(f'Invalid repo path ({str(e)})')
+    return _p
+
+def validate_dir(_path):
+    if not _path: return False 
+    if not os.path.isdir(_path):
+        if os.path.isfile(_path):
+            raise FileNotFoundError('The path specified is a file, not a directory')
+        return False
+    return Tru1e
+
 
 if __name__ == '__main__':
     main()
